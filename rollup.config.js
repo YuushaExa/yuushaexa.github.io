dff12964854bf0f7c7d57af5433fcff7a6de9089
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-01-17 03:29:09 +0300

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import css from 'rollup-plugin-import-css';
+
+export default {
+  input: 'src/codeMirror.js',
+  output: {
+    file: 'public/codeMirror/codeMirror.bundle.js',
+    format: 'iife',
+    name: 'CodeMirrorBundle',
+  },
+  plugins: [
+    nodeResolve(),
+    css({
+      output: 'codeMirror.bundle.css', 
+    }),
+  ],
+};
