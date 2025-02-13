42cb454ed5c039835798c1894e2c5b67cf69628b
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-02-13 23:08:10 +0300

    Update ssg.js

--- ssg.js
+++ ssg.js
@@ -1,7 +1,6 @@
 const fs = require('fs').promises;
 const path = require('path');
-
-// Define the base URL
+const templates = require('./templates');
 const baseurl = 'https://yuushaexa.github.io/'; // You can change this to any base URL
 
 const dirs = {
@@ -88,114 +87,6 @@ async function generateSpecialPages(partials) {
   ]);
   console.log('Generated: index.html and 404.html');
 }
-
-// Define templates
-const templates = {
-  gamesTemplate: {
-    generateSubforumPage: (subforum, baseurl) => `
-      <h1>${subforum.title}</h1>
-      <p>${subforum.description}</p>
-      <p>${subforum.created_at}</p>
-      <img src="${subforum.banner}" alt="${subforum.title}">
-      <img src="${subforum.icon}" alt="${subforum.title}">
-      <ul>${subforum.posts.map(post => `
-        <li>
-          <img src="${post.image}" alt="${post.title}" width="50">
-          <a href="${post.link}">${post.title}</a>
-          <span>(${post.flair})</span>
-          <br>By ${post.author} on ${post.date}
-        </li>
-      `).join('')}</ul>
-    `,
-
-    generatePostPage: (post, subforum, baseurl) => `
-      <h1>${post.title}</h1>
-      <p>By ${post.author} on ${post.date}</p>
-      <img src="${post.image}" alt="${post.title}" width="200">
-      <p>${post.content}</p>
-      <p><a href="${post.url}" target="_blank">Read more</a></p>
-    `,
-
-    generateRSSFeed: (subforum, baseurl) => {
-      const feedUrl = `${baseurl}${subforum.link.replace(/^\//, '')}.rss`;
-      const items = subforum.posts.map(post => `
-        <item>
-          <title>${post.title}</title>
-          <link>${baseurl}${post.link.replace(/^\//, '')}.html</link>
-          <description>${post.content || ''}</description>
-          <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-          <guid>${baseurl}${post.link.replace(/^\//, '')}.html</guid>
-        </item>
-      `).join('');
-
-      return `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-  <channel>
-    <title>${subforum.title}</title>
-    <link>${baseurl}${subforum.link.replace(/^\//, '')}.html</link>
-    <description>${subforum.description}</description>
-    <atom:link href="${feedUrl}" rel="self" type="application/rss+xml" />
-    <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-    ${items}
-  </channel>
-</rss>`;
-    },
-  },
-
-  programmingTemplate: {
-    generateSubforumPage: (subforum, baseurl) => `
-      <h1>${subforum.title}</h1>
-      <p>${subforum.description}</p>
-      <p>${subforum.created_at}</p>
-      <img src="${subforum.banner}" alt="${subforum.title}">
-      <img src="${subforum.icon}" alt="${subforum.title}">
-      <ul>${subforum.posts.map(post => `
-        <li>
-          <img src="${post.image}" alt="${post.title}" width="50">
-          <a href="${post.link}">${post.title}</a>
-          <span>(${post.flair})</span>
-          <br>By ${post.author} on ${post.date}
-          <p>${post.content}</p>
-        </li>
-      `).join('')}</ul>
-    `,
-
-    generatePostPage: (post, subforum, baseurl) => `
-      <h1>${post.title}</h1>
-      <p>By ${post.author} on ${post.date}</p>
-      <img src="${post.image}" alt="${post.title}" width="200">
-      <p>${post.content}</p>
-      <p><a href="${post.url}" target="_blank">Read more</a></p>
-      <p><strong>Related to:</strong> ${subforum.title}</p>
-    `,
-
-    generateRSSFeed: (subforum, baseurl) => {
-      const feedUrl = `${baseurl}${subforum.link.replace(/^\//, '')}.rss`;
-      const items = subforum.posts.map(post => `
-        <item>
-          <title>${post.title}</title>
-          <link>${baseurl}${post.link.replace(/^\//, '')}.html</link>
-          <description>${post.content || ''}</description>
-          <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-          <guid>${baseurl}${post.link.replace(/^\//, '')}.html</guid>
-          <category>${post.flair}</category>
-        </item>
-      `).join('');
-
-      return `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-  <channel>
-    <title>${subforum.title}</title>
-    <link>${baseurl}${subforum.link.replace(/^\//, '')}.html</link>
-    <description>${subforum.description}</description>
-    <atom:link href="${feedUrl}" rel="self" type="application/rss+xml" />
-    <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-    ${items}
-  </channel>
-</rss>`;
-    },
-  },
-};
 
 async function generateSubforumPages(partials, subforums) {
   await Promise.all(Object.entries(subforums).map(async ([key, subforum]) => {
