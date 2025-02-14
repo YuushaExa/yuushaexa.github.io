fae81055584d99efc9046954b87dd416a5652ffd
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-02-15 00:12:03 +0300

    Update templates.js

--- templates.js
+++ templates.js
@@ -117,8 +117,9 @@ const templates = {
       <p>${subforum.created_at}</p>
       <img src="${subforum.banner}" alt="${subforum.title}">
       <img src="${subforum.icon}" alt="${subforum.title}">
-      <ul>${subforum.posts.map(post => `
-        <li>
+  <ul>${subforum.posts.map(post => {
+          const title = post.title || 'untitled'; // Fallback to 'untitled' if title is missing
+          return `        <li>
           <img src="${post.image}" alt="${post.title}" width="50">
           <a href="${post.link}">${post.title}</a>
           <span>(${post.flair})</span>
