1b865b415234284cb3dd7efb84be0fb4ff3cca1a
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-02-16 00:38:35 +0300

    Update templates.js

--- templates.js
+++ templates.js
@@ -160,6 +160,60 @@ const templates = {
 </rss>`;
     },
   },
+
+   vnTemplate: {
+    generatePostLink: (subforumKey, post) => `/${subforumKey}/${generateSlug(post.title)}`,
+
+    generateSubforumPage: (subforum, baseurl) => `
+      <h1>${subforum.title}</h1>
+      <p>${subforum.description}</p>
+      <p>${subforum.created_at}</p>
+      <img src="${subforum.banner}" alt="${subforum.title}">
+      <img src="${subforum.icon}" alt="${subforum.title}">
+      <ul>${subforum.posts.map(post => `
+        <li>
+          <img src="${post.image}" alt="${post.title}" width="50">
+          <a href="${post.link}">${post.title}</a>
+          <span>(${post.flair})</span>
+          <br>By ${post.author} on ${post.date}
+        </li>
+      `).join('')}</ul>
+    `,
+
+    generatePostPage: (post, subforum, baseurl) => `
+      <h1>${post.title}</h1>
+      <p>By ${post.author} on ${post.date}</p>
+      <img src="${post.image}" alt="${post.title}" width="200">
+      <p>${post.content}</p>
+      <p><a href="${post.url}" target="_blank">Read more</a></p>
+    `,
+
+    generateRSSFeed: (subforum, baseurl) => {
+      const feedUrl = `${baseurl}${subforum.link.replace(/^\//, '')}.rss`;
+      const items = subforum.posts.map(post => `
+        <item>
+          <title>${post.title}</title>
+          <link>${baseurl}${post.link.replace(/^\//, '')}.html</link>
+          <description>${post.content || ''}</description>
+          <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+          <guid>${baseurl}${post.link.replace(/^\//, '')}.html</guid>
+        </item>
+      `).join('');
+
+      return `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>${subforum.title}</title>
+    <link>${baseurl}${subforum.link.replace(/^\//, '')}.html</link>
+    <description>${subforum.description}</description>
+    <atom:link href="${feedUrl}" rel="self" type="application/rss+xml" />
+    <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+    ${items}
+  </channel>
+</rss>`;
+    },
+  },
+  
 };
 
 // Helper function to generate slugs
